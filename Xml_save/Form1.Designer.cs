9f2636a33c5236d957564b63ec131926ad4ea2c3
3a9lu <eduard.grinvald@yandex.ru>
2021-09-12 15:50:47 +0700

    Всё готово.

---
+++ Xml_save/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace Xml_save
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Налог = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(284, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(542, 40);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1661992959,
+            1808227885,
+            5,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(216, 27);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(542, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(216, 68);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Сохранить в XML";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(542, 370);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(216, 68);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Загрузить из XML";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(284, 370);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(216, 29);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Очистить таблицу";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Налог});
+            this.dataGridView1.Location = new System.Drawing.Point(130, 138);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(555, 188);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Фамилия";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Имя";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Доход";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Налог
+            // 
+            this.Налог.HeaderText = "Налог";
+            this.Налог.MinimumWidth = 6;
+            this.Налог.Name = "Налог";
+            this.Налог.ReadOnly = true;
+            this.Налог.Width = 125;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(284, 332);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(216, 29);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Подсчитать налог";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(71, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 28);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(367, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 28);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(615, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 28);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Доход";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(781, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Запись в XML, загрузка и подсчёт";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Налог;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Xml_save/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xml_save
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;  // Вывод формы по центру экрана
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // Добавление данных в форму
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Заполните все поля!", "Ошибка");
+            }
+            else
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = textBox1.Text; // Фамилия
+                dataGridView1.Rows[n].Cells[1].Value = textBox2.Text; // Имя
+                dataGridView1.Rows[n].Cells[2].Value = numericUpDown1.Value; // Доход
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet(); // Создаем пустой кэш данных
+                DataTable dt = new DataTable(); // Создаем пустую таблицу данных
+                dt.TableName = "Report"; // Название таблицы
+                dt.Columns.Add("Surname"); // Название колонок
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Income");
+                ds.Tables.Add(dt); // В ds создается таблица, с названием и колонками
+
+                foreach (DataGridViewRow r in dataGridView1.Rows) //Пока в dataGridView есть строки
+                {
+                    DataRow row = ds.Tables["Report"].NewRow(); // Создаем новую строку в таблице, занесенной в ds
+                    row["Surname"] = r.Cells[0].Value;  // В столбец этой строки заносим данные из первого столбца dataGridView1
+                    row["Name"] = r.Cells[1].Value; // Со вторыми столбцами
+                    row["Income"] = r.Cells[2].Value; // С третьими столбцами
+                    ds.Tables["Report"].Rows.Add(row); // Добавление всей этой строки в таблицу ds
+                }
+                ds.WriteXml("D:\\Data.xml");
+                MessageBox.Show("XML файл успешно сохранен!", "Выполнено");
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно сохранить XML файл!", "Ошибка");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0) // Если в таблице больше нуля строк
+            {
+                MessageBox.Show("Очистите поле перед загрузкой нового файла!", "Ошибка");
+            }
+            else
+            {
+                if (File.Exists("D:\\Data.xml")) // Если существует данный файл
+                {
+                    DataSet ds = new DataSet(); // Создаем пустой кэш данных
+                    ds.ReadXml("D:\\Data.xml"); // Записываем в него XML-данные из файла
+
+                    foreach (DataRow item in ds.Tables["Report"].Rows)
+                    {
+                        int n = dataGridView1.Rows.Add(); // Добавляем новую сроку в dataGridView
+                        dataGridView1.Rows[n].Cells[0].Value = item["Surname"]; // Заносим в первый столбец созданной строки данные из первого столбца таблицы ds
+                        dataGridView1.Rows[n].Cells[1].Value = item["Name"]; // Со вторыми столбцами
+                        dataGridView1.Rows[n].Cells[2].Value = item["Income"]; // С третьими столбцами
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("XML файл не найден!", "Ошибка");
+                }
+            }
+        }
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            int n = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[2].Value);
+            numericUpDown1.Value = n;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Редактирование
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int n = dataGridView1.SelectedRows[0].Index;
+                dataGridView1.Rows[n].Cells[0].Value = textBox1.Text;
+                dataGridView1.Rows[n].Cells[1].Value = textBox2.Text;
+                dataGridView1.Rows[n].Cells[2].Value = numericUpDown1.Value;
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для редактирования!", "Ошибка");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Удаление
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для удаления!", "Ошибка");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Очистка таблицы
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                dataGridView1.Rows.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Таблица пустая!", "Ошибка");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Подсчёт налога
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int n = dataGridView1.SelectedRows[0].Index;
+                if (numericUpDown1.Value == 0)
+                {
+                    MessageBox.Show("Выберите строку и продолжите снова!", "Ошибка");
+                }
+                if (numericUpDown1.Value < 20000)
+                {
+                    dataGridView1.Rows[n].Cells[3].Value = numericUpDown1.Value /100 * 12;
+                }
+                else if (numericUpDown1.Value > 20000 && numericUpDown1.Value < 40000)
+                {
+                    dataGridView1.Rows[n].Cells[3].Value = numericUpDown1.Value / 100 * 20;
+                }
+                else if (numericUpDown1.Value > 40000)
+                {
+                    dataGridView1.Rows[n].Cells[3].Value = numericUpDown1.Value / 100 * 35;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для редактирования!", "Ошибка");
+            }
+        }
+    }
+}
